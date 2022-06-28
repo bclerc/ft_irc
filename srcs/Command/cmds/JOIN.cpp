d507fecf4e08d1865f4bda9f73b11986d67bc928
safernan <safernan@student.42.fr>
2022-06-28 07:08:29 +0200

    save

--- srcs/Command/cmds/JOIN.cpp
+++ srcs/Command/cmds/JOIN.cpp
@@ -3,25 +3,27 @@
 void joinCommand(CommandManager::Command & command)
 {
 	User & sender = *command.sender;
-  std::string RPL_NAMERPL = ":";
+ 	std::string RPL_NAMERPL;
 	Channel & new_channel = server.isChannel(command.args[0]) ?  server.getChannel(command.args[0]) : server.createChannel(command.args[0], *command.sender);	
-	
+
 	new_channel.addUser(*command.sender);
-	sender.send(":" + sender.getNick() + " JOIN " + new_channel.getName());
+	sender.send(":" + sender.getNick() + " JOIN :" + new_channel.getName());
 
-  const_iterator it = new_channel.getUsers().begin();
+  	const_iterator it = new_channel.getUsers().begin();
 
-    RPL_NAMERPL +=" 353 " + sender.getNick();
-    RPL_NAMERPL += " " + new_channel.getName() + " :";
+    RPL_NAMERPL +="353 " + sender.getNick() + " " + new_channel.getName() + " :";
+
     while (it != new_channel.getUsers().end())
     {
-	    RPL_NAMERPL += " ";
+		RPL_NAMERPL += " ";
 	    if (new_channel.isOperator(*it))
-	      RPL_NAMERPL += "@";
-    	RPL_NAMERPL += it->getNick();
+	      RPL_NAMERPL += "@" + it->getNick();
+		else
+    		RPL_NAMERPL +="+" + it->getNick();
     	++it;
     }
-   
+   //sender.send(": 332 " + sender.getNick() + " " + new_channel.getName() + " :");
    sender.send(RPL_NAMERPL);
+   sender.send("366 " + sender.getNick() + " " + new_channel.getName() + " :End of NAMES list");
    
 }
