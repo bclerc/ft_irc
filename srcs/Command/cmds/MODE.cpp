a033439a1aa8f5bb46408bfb94bf89444a764710
BuildTools <unconfigured@null.spigotmc.org>
2022-07-04 15:40:22 +0000

    RPL

--- srcs/Command/cmds/MODE.cpp
+++ srcs/Command/cmds/MODE.cpp
@@ -1,80 +1,121 @@
 # include "../CommandManager.hpp"
 
-void user_mode(CommandManager::Command const & command, User & sender)
+void oper(User & sender, User & target, int plus)
 {
-	User & target = server.getUser(command.args[0]);
-	if (command.size < 3)
-	{
-		if (sender != target)
-		{
-			sender.send(ERR_USERSDONTMATCH(sender.getName()));
-			return ;
-		}
-		sender.send(RPL_UMODEIS(target.getName(), target.isOperator() ? "+o" : ""));
-		return ;
-	}
-	if (sender.isOperator())
-	{
-		if (command.args[1] == "+o")
-			target.setOperator(true);
-		else if (command.args[1] == "-o")
-			target.setOperator(false);
-		else {
-			sender.send(ERR_UMODEUNKNOWNFLAG(sender.getName(), command.args[1].c_str()[0]));
-			return ;
-		}
-		if (target != sender)
-			sender.send(":" + target.getPrefix() + " MODE " + target.getName() + " " + command.args[1]);
-		target.send(":" + target.getPrefix() + " MODE " + target.getName() + " " + command.args[1]);
-	}
-	else
-		sender.send(ERR_NOPRIVILEGES(sender.getName()));
+    if (sender.isOperator()) {
+        target.setOperator(plus);
+        sender.send(":" + sender.getName() + " MODE " + target.getName() + (plus ? " +o": " -o"));
+        if (sender != target)
+            target.send(":" + sender.getName() + " MODE " + target.getName() + (plus ? " +o": " -o"));
+    } else
+        sender.send(ERR_NOPRIVILEGES(sender.getName()));
 }
 
-void channel_mode(CommandManager::Command const & command, User & sender)
+void oper(User & sender, User & target, Channel & channel, int plus)
 {
-	Channel & channel = server.getChannel(command.args[0]);
-	if (command.size < 3)
+    if (channel.isOperator(sender)) {
+		channel.setOperator(sender, plus);
+        sender.send(":" + sender.getName() + " MODE " + channel.getName() + " " + target.getName() + (plus ? " +o": " -o"));
+        if (sender != target)
+            target.send(":" + sender.getName() + " MODE " + channel.getName() + " " + target.getName() + (plus ? " +o": " -o"));
+    } else
+        sender.send(ERR_CHANOPRIVSNEEDED(sender.getName(), channel.getName()));
+}
+
+void mode_channel(CommandManager::Command & command, User & sender)
+{
+	const char * mode = command.args[1].c_str();
+	User * target = &sender;
+	int		plus = 1;
+
+	if (command.size == 2)
 	{
-		sender.send("324 " + sender.getName() + " " + channel.getName() + " +n");
+		sender.send(":" + sender.getPrefix() + " 324 " + sender.getName() + " " + command.args[0] + " +n");
+		sender.send(":" + sender.getPrefix() + " 315 " + sender.getName() + " " + command.args[0] + " :End of /WHO list.");
 		return ;
 	}
-	User & target = command.size == 3 ? server.getUser(command.args[2]) : sender;
-	if (channel.isOperator(sender))
+
+	if (command.args.size() == 3)
 	{
-		if (command.args[1] == "+o")
-			channel.setOperator(target, true);
-		else if (command.args[1] == "-o")
-			channel.setOperator(target, false);
-		else {
-			sender.send(ERR_UMODEUNKNOWNFLAG(sender.getName(), command.args[1].c_str()[0]));
+		if (server.isUser(command.args[2]))
+			target = &server.getUser(command.args[2]);
+		else
+		{
+			sender.send(ERR_NOSUCHNICK(sender.getName(), command.args[2]));
 			return ;
 		}
-		target.send(":" + sender.getName() + " MODE " + channel.getName() + " " + command.args[1] + " " + target.getName());
-		if (sender != target)
-			sender.send(":" + sender.getName() + " MODE " + channel.getName() + " " + command.args[1] + " " + target.getName());
-	} else
-		sender.send(ERR_CHANOPRIVSNEEDED(sender.getName(), channel.getName()));
+	}
+	if (server.isChannel(command.args[0]))
+	{
+		Channel & channel = server.getChannel(command.args[0]);
+	   
+	    for (int i = 0; mode[i]; i++)
+    	{
+			switch (mode[i])
+			{
+				case '+':
+					plus = 1;
+					break ;
+				case '-':
+					plus = 0;
+					break ;
+				case 'o':
+					oper(sender, *target, channel, plus);
+					break;
+				default:
+					sender.send(ERR_UMODEUNKNOWNFLAG(sender.getName(), mode[i]));
+			}
+    	}
+
+	}
+	else
+		sender.send(ERR_NOSUCHCHANNEL(sender.getName(), command.args[0]));
+}
+
+void mode_user(CommandManager::Command & command, User & sender)
+{
+	const char	*mode = command.args[1].c_str();
+
+	if (!server.isUser(command.args[0]))
+    {
+        sender.send(ERR_NOSUCHNICK(sender.getName(), command.args[0]));
+        return ;
+    }
+
+	User		&target = server.getUser(command.args[0]);
+	int			plus = 1;
+
+    for (int i = 0; mode[i]; i++)
+    {
+        switch (mode[i])
+        {
+            case '+':
+                plus = 1;
+                break ;
+            case '-':
+                plus = 0;
+                break ;
+            case 'o':
+                oper(sender, target, plus);
+                break;
+            default:
+                sender.send(ERR_UMODEUNKNOWNFLAG(sender.getName(), mode[i]));
+        }
+    }
 }
 
 void modeCommand(CommandManager::Command & command)
 {
 	User & sender = *command.sender;
-	if (command.args.size() < 1)
-	{
-		sender.send(ERR_NEEDMOREPARAMS(sender.getName(), command.command));
-		return ;
-	}
-	try
-	{
-		if (command.args[0][0] == '#')
-			channel_mode(command, sender);
-		else
-			user_mode(command, sender);
-	} catch (Server::UserNotFoundException & e) {
-		sender.send(ERR_NOSUCHNICK(sender.getName(), command.args[0]));
-	} catch (Server::ChannelNotFoundException & e) {
-		sender.send(ERR_NOSUCHCHANNEL(sender.getName(), command.args[0]));
-	}
-}
 
+	 if (command.size < 2)
+    {
+        sender.send(ERR_NEEDMOREPARAMS(sender.getName(), command.command));
+        return ;
+    }
+	if (command.args[0].at(0) == '#')
+		mode_channel(command, sender);
+	else
+		mode_user(command, sender);
+	return ;
+}
